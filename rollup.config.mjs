d22b5a4e5406bcebc405ec45f43e81188dfcfdb3
ito-ito <ittooco@gmail.com>
2026-05-28 21:55:43 +0900

    initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const disableEntryPointTreeShaking = () => ({
+  name: 'no-treeshaking',
+  async resolveId(source, importer, options) {
+    if (!importer) {
+      const resolution = await this.resolve(source, importer, { skipSelf: true, ...options });
+      resolution.moduleSideEffects = 'no-treeshake';
+      return resolution;
+    }
+    return null;
+  },
+  renderChunk(code) {
+    return code.replace(/\nexport\s+\{.*\};/g, '');
+  },
+});
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+  },
+  plugins: [
+    disableEntryPointTreeShaking(),
+    nodeResolve(),
+    commonjs(),
+    typescript(),
+  ],
+};
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// エントリーポイント
+// GAS の doPost など公開関数はここから export する
+
+declare const global: {
+  doPost: (e: GoogleAppsScript.Events.DoPost) => GoogleAppsScript.Content.TextOutput;
+};
+
+global.doPost = (e) => {
+  return ContentService.createTextOutput(
+    JSON.stringify({ status: 'ok' })
+  ).setMimeType(ContentService.MimeType.JSON);
+};
